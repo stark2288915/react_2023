2999fa09fddfa049e994b73f2d98e28c22efb0ae
stark2288915 <stark2288915@gamil.com>
2024-02-05 21:49:00 +0200

    sasf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {LectionComponent} from "./components/LectionComponent/lectionComponent";
+import {UserPostFormComponent} from "./components/UsersComponent/UserPostFormComponent";
 function App() {
 
     return (
     <div className="App">
-        <LectionComponent/>
+        {/*<LectionComponent/>*/}
+        <UserPostFormComponent/>
     </div>
   );
 }
---
+++ src/components/UsersComponent/UserPostFormComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const UserPostFormComponent = () => {
+    return (
+        <div>
+            <form onSubmit={}>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+                <input type="text"/>
+            </form>
+        </div>
+    );
+};
+
+export {UserPostFormComponent};
